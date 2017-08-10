4213773986144347933983b6f686a5e77cb9904c
Unknown <zeichnerwunder21@web.de>
2017-08-10 13:50:45 +0200

    functions added

--- src/src.ino
+++ src/src.ino
@@ -1,9 +1,90 @@
+#include <Servo_Manual.h>
+#include <IR_Keys.h>
+#include <IRremote.h>
+#include <Robot_Motor.h>
+#include <SR04_Ultrasonic.h>
+
+  //inital
+  Robot_Motor *flash = new Robot_Motor (5, 6, 9, 10);
+  SR04_Ultrasonic *sonic = new SR04_Ultrasonic (13, A0);
+  Servo_Manual *serv = new Servo_Manual (11);
+
+  //variabel Colors
+  const short sensL = 2;
+  const short sensR = 3;
+
+  bool left = false;
+  bool right = false;
+
+  void getColors ();
+  void findLeftBack ();
+  void findRight ();
+  
+  //variabel colliding
+  const short drehZeit = 25;
+  const short longTurn = 200;
+  const float collEntf = 20.0; //collisions-entfernung in cm
+
+  bool isColliding ();
+  
+  
 void setup() {
-  // put your setup code here, to run once:
+  flash->setSpeed(150);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  //Colors
 
 }
+
+//functions
+void getColors () {
+  if (digitalRead (sensL) == LOW) {
+    left = true;
+  } else {
+    left = false;
+  }
+
+  if (digitalRead (sensR) == LOW) {
+    right = true;
+  } else {
+    right = false;
+  }
+}
+
+void findLeftBlack () {
+  while (right == false) {
+    flash->turnRight (10);
+    getColors ();
+  }
+  flash->drive (100);
+}
+
+void findRightBlack () {
+  while (left == false) {
+    flash->turnLeft (10);
+    getColors ();
+  }
+  flash->drive (100);
+}
+
+// colliding
+bool isColliding () {
+   if (sens-> measureDistance () < collEntf) {
+      writeCollisionMessage();
+      return true;
+   }
+   else {
+      lcd -> clear();
+      return false;
+   }
+}
+
+
+
+
+
+
+
