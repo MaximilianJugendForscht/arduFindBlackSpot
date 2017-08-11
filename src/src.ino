6e249718936c88f57ecbb84db337829769fb2742
Unknown <wittmaxi@outlook.de>
2017-08-11 12:32:33 +0200

    s

--- src/src.ino
+++ src/src.ino
@@ -1,14 +1,12 @@
 #include <Servo_Manual.h>
 #include <Robot_Motor.h>
 #include <SR04_Ultrasonic.h>
-#include <LiquidCrystal.h>
-
+#include <LiquidCrystal_I2C.h>
   //initalizing
   Robot_Motor *flash = new Robot_Motor (5, 6, 9, 10);
   SR04_Ultrasonic *sonic = new SR04_Ultrasonic (13, A0);
   Servo_Manual *serv = new Servo_Manual (11);
-  LiquidCrystal *lcd = new LiquidCrystal (0x27, 2, 1, 0, 4, 5, 6, 7);
-
+  LiquidCrystal_I2C *lcd = new LiquidCrystal_I2C (0x27, 2, 1, 0, 4, 5, 6, 7);
   //variable Colors
   const short sensL = 2;
   const short sensR = 3;
@@ -18,6 +16,8 @@
   int startedTimer;
   int endedTimer;
 
+  const int turnTime = 300;
+
   int toDrive;
 
   bool left = false;
@@ -25,31 +25,38 @@
 
   void goToNewPlace ();
   void getColors ();
-  void findLeftBack ();
-  void findRight ();
+  void findLeftBlack ();
+  void findRightBlack ();
   void goToNewPlace ();
-  void checkServo();
+  void spiralDrehung();
+  void findBestDirection();
+  bool isColliding();
 
   //variable colliding
   const short drehZeit = 25;
   const short longTurn = 200;
-  const float collEntf = 10.0; //collisions-entfernung in cm
+  const float collEntf = 20.0; //collisions-entfernung in cm
 
   int getDriveTime ();
 
 void setup() {
-  flash->setSpeed(150);
+  flash->setSpeed(215);
   serv ->setPosition (255, 40);
+  lcd -> setBacklightPin (3, POSITIVE);
+  lcd -> setBacklight (HIGH);
+  lcd -> begin(16, 2);
 }
 
 void loop() {
-  if (!findRightBlack && !findLeftBlack) {
+  if (!(left) && !(right)) {
     serv -> setPosition (255, 40);
     spiralDrehung();
   }
-  getColors();
+  lcd -> setCursor (10, 0);
+  lcd -> print ("HALLO");
+  /*getColors();
   findRightBlack();
-  findLeftBlack ();
+  findLeftBlack ();*/
 }
 
 //functions
@@ -73,7 +80,7 @@ void getColors () {
 
 void findLeftBlack () {
   while (right == false) {
-    flash->turnRight (10);
+    flash->turnLeft (10);
     getColors ();
   }
   flash->drive (100);
@@ -81,7 +88,7 @@ void findLeftBlack () {
 
 void findRightBlack () {
   while (left == false) {
-    flash->turnLeft (10);
+    flash->turnRight (10);
     getColors ();
   }
   flash->drive (100);
@@ -89,7 +96,7 @@ void findRightBlack () {
 
 
 int getDriveTime () {
-  return timetoCollide;
+  return endedTimer - startedTimer ;
 }
 
 void startTimer () {
@@ -100,65 +107,134 @@ void endTimer () {
   endedTimer = millis();
 }
 
-void SpiralDrehung () {
-  startTimer ();
-  while (sonic->measureDistance() > collEntf) {
-    motor.turnLeft(5);
-    motor.drive(10);
+void spiralDrehung () {
+  serv-> setPosition (50, 50);
+  int counter = 40;
+  int whileCounter = 0;
+  bool lookLeft = false;
+  while (!(isColliding())) {
+    flash->turnLeft(50);
+    flash->drive (counter);
+    counter += 1;
+    if (lookLeft = false) {
+      serv -> setPosition (190, 10);
+    } else  {
+      serv -> setPosition (30, 10);
+    }
   }
-  endTimer ();
-  toDrive = getDriveTime() / 2;
+  flash -> turnLeft (turnTime);
   goToNewPlace ();
 }
 
 void goToNewPlace () {
-  int start = millis ();
+  serv -> setPosition (90, 10);
   startTimer ();
-  while (start - millis <= toDrive) {
+  while (!(isColliding())) {
     checkServo();
-    flash->forward();
-    if (sonic-> measureDistance() > collEntf) {
-      flash -> stop();
-      flash -> turnLeft (500); //to be tested at SAP
-      endTimer();
-      break;
+  }
+  flash->stop();
+  flash->turnRight(turnTime);
+  endTimer ();
+  toDrive = getDriveTime();
+  toDrive = toDrive / 2;
+  
+  int start = millis ();
+  lcd->clear();
+  lcd -> home();
+  lcd->print (start);
+  lcd -> setCursor (1, 2);
+  lcd->print (toDrive);
+  startTimer ();
+  while (millis () -start <= toDrive) {
+    lcd -> setCursor(10, 1);
+    lcd -> print ("DEB");
+    checkServo();
+    if (isColliding()) {
+      endTimer ();
+      toDrive = getDriveTime ();
+      toDrive = toDrive * (2/3);
+      startTimer ();
     }
   }
-  motor.stop ();
+  flash->stop ();
 }
 
-void checkServo () {
-   serv->setPosition (30, 15);
-   if (sens->measureDistance() > collEntf){
-         flash ->forward ();
-      }
-      else {
-        flash->stop();
-        findBestDirection();
-      }
-   serv->setPosition (90, 15);
-   if (!(isColliding())){
-         flash->forward ();
-      }
-      else {
-        flash->stop();
-        findBestDirection();
-      }
-  serv->setPosition (150, 15);
-   if (sens->measureDistance() > collEntf){
-         flash->forward ();
-      }
-      else {
-        flash->stop();
-        findBestDirection();
-      }
+bool isColliding () { 
+   if (sonic-> measureDistance () < collEntf) { 
+      return true; 
+   } 
+   else { 
+      return false; 
+   } 
+} 
 
-   serv->setPosition (90, 15);
-   if (!(isColliding())){
-         flash->forward ();
-      }
-      else {
-        flash ->stop();
+void checkServo () { 
+   serv->setPosition (0, 20); 
+   if (sonic->measureDistance() > collEntf){ 
+         flash ->forward (); 
+      } 
+      else { 
+        flash->stop(); 
         findBestDirection();
-      }
-}
+        toDrive = toDrive * (2/3); 
+      } 
+   serv->setPosition (90, 15); 
+   if (!(isColliding())){ 
+         flash->forward (); 
+      } 
+      else { 
+        flash->stop(); 
+        findBestDirection();
+        toDrive = toDrive * (2/3); 
+      } 
+  serv->setPosition (255, 20); 
+   if (sonic->measureDistance() > collEntf){ 
+         flash->forward (); 
+      } 
+      else { 
+        flash->stop(); 
+        findBestDirection();
+        toDrive = toDrive * (2/3); 
+      } 
+ 
+   serv->setPosition (90, 15); 
+   if (!(isColliding())){ 
+         flash->forward (); 
+      } 
+      else { 
+        flash->stop(); 
+        findBestDirection();
+        toDrive = toDrive * (2/3); 
+      } 
+} 
+
+bool evalDist (int a, int b) { 
+  if (a > b) { 
+    return false; 
+  } if (a < b) { 
+    return true; 
+  } 
+  if (a == b) { 
+    return true; 
+  } 
+} 
+ 
+void findBestDirection () { 
+  int leftDist; 
+  int rightDist; 
+ 
+  serv->setPosition (255, 10); 
+  leftDist = sonic-> measureDistance(); 
+  serv->setPosition (0, 10); 
+  rightDist = sonic-> measureDistance(); 
+  bool rightTurn = evalDist (leftDist, rightDist); 
+ 
+  if (rightTurn && rightDist < collEntf) { 
+    flash->turnRight (600); 
+    delay (600); 
+  } else if (!(rightTurn) && leftDist < collEntf) { 
+    flash->turnLeft (600); 
+    delay(600); }
+} 
+ 
+
